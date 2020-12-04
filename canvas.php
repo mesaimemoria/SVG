0d32ea147a086b6974a2e43afce7e3ae47347e0c
mesaimemoria <50259637+mesaimemoria@users.noreply.github.com>
2020-12-04 18:40:28 +0800

    Add files via upload

---
+++ canvas.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>HTML5 Page</title>
+  </head>
+  <body>
+    <canvas id="canvas1" width="1000" height="1000">
+  </body>
+</html>
+
+<script type="text/javascript" src="canvas1.js"></script>
---
+++ canvas1.js
@@ -0,0 +1,218 @@
+var canvas = document.getElementById("canvas1");
+	var ctx = canvas.getContext("2d");
+	
+	ctx.beginPath();
+	ctx.moveTo(40,40);
+	ctx.lineTo(100,75);
+	ctx.lineTo(160,40);
+	ctx.lineTo(100,150);
+	ctx.lineTo(40,40);
+	ctx.closePath();
+	ctx.fillStyle = "#7ae9ff";
+	ctx.fill();
+	
+	
+	ctx.beginPath();
+	ctx.moveTo(100,75);
+	ctx.lineTo(160,40);
+	ctx.lineTo(100,150);
+	ctx.lineTo(100,75);
+	ctx.closePath();
+	ctx.fillStyle = "#25b0cc";
+	ctx.fill();
+	
+	ctx.beginPath();
+	ctx.moveTo(230,50);
+	ctx.lineTo(370,50);
+	ctx.lineTo(370,130);
+	ctx.lineTo(230,130);
+	ctx.lineTo(230,50);
+	ctx.closePath();
+	ctx.fillStyle = "#bf2650";
+	ctx.fill();
+	
+	ctx.beginPath();
+	ctx.moveTo(230,50);
+	ctx.lineTo(370,50);
+	ctx.lineTo(300,100);
+	ctx.lineTo(230,50);
+	ctx.closePath();
+	ctx.fillStyle = "#ff7a9e";
+	ctx.fill();
+	
+	ctx.beginPath();
+	ctx.moveTo(440,70);
+	ctx.lineTo(500,30);
+	ctx.lineTo(560,70);
+	ctx.lineTo(540,70);
+	ctx.lineTo(540,130);
+	ctx.lineTo(460,130);
+	ctx.lineTo(460,70);
+	ctx.lineTo(440,70);
+	ctx.closePath();
+	ctx.fillStyle = "#579e1c";
+	ctx.fill();
+	
+	
+	ctx.beginPath();
+	ctx.moveTo(485,130);
+	ctx.lineTo(485,90);
+	ctx.lineTo(515,90);
+	ctx.lineTo(515,130);
+	ctx.lineTo(485,130);
+	ctx.closePath();
+	ctx.fillStyle = "#90de50";
+	ctx.fill();
+	
+	ctx.beginPath();
+	ctx.moveTo(630,50);
+	ctx.lineTo(770,50);
+	ctx.lineTo(770,140);
+	ctx.lineTo(630,140);
+	ctx.lineTo(630,50);
+	ctx.closePath();
+	ctx.fillStyle = "#283d8a";
+	ctx.fill();
+	
+	ctx.beginPath();
+	ctx.moveTo(640,60);
+	ctx.lineTo(780,60);
+	ctx.lineTo(780,150);
+	ctx.lineTo(640,150);
+	ctx.lineTo(640,60);
+	ctx.closePath();
+	ctx.fillStyle = "#7e91d6";
+	ctx.fill();
+	
+	ctx.beginPath();
+	ctx.moveTo(20,225);
+	ctx.lineTo(170,225);
+	ctx.lineTo(170,305);
+	ctx.lineTo(110,305);
+	ctx.lineTo(110,315);
+	ctx.lineTo(120,315);
+	ctx.lineTo(120,325);
+	ctx.lineTo(80,325);
+	ctx.lineTo(80,315);
+	ctx.lineTo(90,315);
+	ctx.lineTo(90,305);
+	ctx.lineTo(20,305);
+	ctx.closePath();
+	ctx.fillStyle = "#283d8a";
+	ctx.fill();
+	
+	ctx.beginPath();
+	ctx.rect(28, 232, 135, 65);
+	ctx.fillStyle = "#7e91d6";
+	ctx.fill();
+	
+	ctx.beginPath();
+	ctx.arc(295, 287, 68, 0, 2 * Math.PI);
+	ctx.fillStyle = "black";
+	ctx.fill();
+	
+	ctx.beginPath();
+	ctx.arc(310, 302, 68, 0, 2 * Math.PI);
+	ctx.fillStyle = "#B3B3B3";
+	ctx.fill();
+	
+	ctx.beginPath();
+	ctx.moveTo(580,219);
+	ctx.lineTo(417,219);
+	ctx.lineTo(417,381);
+	ctx.lineTo(523,381);
+	ctx.lineTo(523,332);
+	ctx.lineTo(467,332);
+	ctx.lineTo(467,268);
+	ctx.lineTo(580,268);
+	ctx.closePath();
+	ctx.fillStyle = "#283d8a";
+	ctx.fill();
+	
+	ctx.beginPath();
+	ctx.moveTo(474,276);
+	ctx.lineTo(474,325);
+	ctx.lineTo(531,325);
+	ctx.lineTo(530,381);
+	ctx.lineTo(580,381);
+	ctx.lineTo(580,276);
+	ctx.closePath();
+	ctx.fillStyle = "#283d8a";
+	ctx.fill();
+	
+	ctx.beginPath();
+	ctx.rect(647.55, 219, 105.45, 157);
+	ctx.fillStyle = "#E9C500";
+	ctx.fill();
+	
+	ctx.beginPath();
+	ctx.rect(659.27, 230.72, 82.01, 133.57);
+	ctx.fillStyle = "white";
+	ctx.fill();
+	
+	
+	// ctx.beginPath();
+	// ctx.moveTo(230,50);
+	// ctx.lineTo(370,50);
+	// ctx.lineTo(300,100);
+	// ctx.lineTo(230,50);
+	// ctx.closePath();
+	// ctx.fillStyle = "#ff7a9e";
+	// ctx.fill();
+	
+	
+	// ctx.beginPath();
+	// ctx.moveTo(120,130);
+	// ctx.lineTo(310,130);
+	// ctx.lineTo(310,270);
+	// ctx.arc(290,270,20,0,1.5);
+	// ctx.lineTo(80,290);
+	// ctx.lineTo(80,170);
+	// ctx.quadraticCurveTo(77,130,120,130);
+	// ctx.closePath();
+	// ctx.fillStyle = "#ff608d";
+	// ctx.fill();
+	
+	
+	// ctx.beginPath();
+	// ctx.moveTo(120,150);
+	// ctx.lineTo(310,150);
+	// ctx.lineTo(310,270);
+	// ctx.arc(290,270,20,0,1.5);
+	// ctx.lineTo(100,290);
+	// ctx.lineTo(100,180);
+	// ctx.quadraticCurveTo(98,150,120,150);
+	// ctx.closePath();
+	// ctx.fillStyle = "#866539";
+	// ctx.fill();
+	
+	// ctx.beginPath();
+	// ctx.moveTo(100,190);
+	// ctx.lineTo(310,190);
+	// ctx.lineTo(310,250);
+	// ctx.lineTo(100,250);
+	// ctx.closePath();
+	// ctx.fillStyle = "#ffd553";
+	// ctx.fill();
+	
+	// ctx.beginPath();
+	// ctx.moveTo(120,100);
+	// ctx.lineTo(150,100);
+	// ctx.quadraticCurveTo(133,45,115,100);
+	// ctx.closePath();
+	// ctx.fillStyle = "#ff6376";
+	// ctx.fill();	
+	
+	// ctx.beginPath();
+	// ctx.moveTo(120,100);
+	// ctx.lineTo(270,100);
+	// ctx.quadraticCurveTo(249,45,233,100);
+	// ctx.closePath();
+	// ctx.fillStyle = "#ff6376";
+	// ctx.fill();	
+
+	
+	// ctx.beginPath();
+	// ctx.arc(60,110,25,0,2*Math.PI);
+	// ctx.fillStyle = "#ff9596";
+	// ctx.fill();
